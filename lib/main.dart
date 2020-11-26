2fe9a1d1e054d0bbb6ea80bd94a892943130225e
jaredbaszler <jaredbaszler@gmail.com>
2020-11-26 01:00:49 -0600

    adding of packages and read excel

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'package:excel/excel.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //runApp(MyApp());
+  // var file = "assets/test_file_2020_11_26.xlsx";
+  // var bytes = File(file).readAsBytesSync();
+  // var excel = Excel.decodeBytes(bytes);
+
+  // for (var table in excel.tables.keys) {
+  //   print(table); //sheet Name
+  //   print(excel.tables[table].maxCols);
+  //   print(excel.tables[table].maxRows);
+  //   for (var row in excel.tables[table].rows) {
+  //     print("$row");
+  //   }
+  // }
+
+  ByteData data = await rootBundle.load("assets/test_file_2020_11_26.xlsx");
+  var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+  var excel = Excel.decodeBytes(bytes);
+
+  for (var table in excel.tables.keys) {
+    print(table); //sheet Name
+    print(excel.tables[table].maxCols);
+    print(excel.tables[table].maxRows);
+    for (var row in excel.tables[table].rows) {
+      print("$row");
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
